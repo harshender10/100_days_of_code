7c0b14bce80c4435a63ff99479ae99a45caed519
harshender10 <69662857+harshender10@users.noreply.github.com>
2023-04-24 22:13:24 +0530

    Update sol_2.cpp

--- Day1/sol_2.cpp
+++ Day1/sol_2.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+         ListNode* temp= head;
+         int count=0;
+         while(temp!=NULL){
+             count++;
+             temp=temp->next;
+         }
 
+            count=count/2;
+            ListNode* ans;
+            int i=0;
+            while(i<=count){
+                ans=head;
+                head=head->next;
+                i++;
+            }
+
+       return ans;
+    }
+};
