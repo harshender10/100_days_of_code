b801857562d017b9d92d0aa0343830e3ba097303
harshender10 <69662857+harshender10@users.noreply.github.com>
2023-04-24 21:42:43 +0530

    Update sol1.cpp

--- Day1/sol1.cpp
+++ Day1/sol1.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
-    ListNode* middleNode(ListNode* head) {
-        
+     int BTD(string s){
+        int dec=0;
+        int base=1;
+        int n= (int)s.length();
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='1')
+                dec+=base;
+            base*=2;
+        }
+        return dec;
+    }
+
+    int getDecimalValue(ListNode* head) {
+        string s;
+        while(head!=NULL){
+            int data= head-> val;
+            char x=(data==1)?'1':'0';
+            s+=x;
+            head=head->next;
+        }
+        int ans= BTD(s);
+        return ans;
     }
 };
